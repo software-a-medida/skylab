3b8bda5b6bf3f7919377e873dbc98462658f240b
Gersón Gómez <gergomez18@gmail.com>
2021-05-03 16:09:30 -0500

    Actualización

--- layouts/Index/index.php
+++ layouts/Index/index.php
@@ -1,11 +1,17 @@
-<?php defined('_EXEC') or die; ?>
+<?php
+
+defined('_EXEC') or die;
+
+$this->dependencies->add(['js', '{$path.js}Index/index.js?v=1.0']);
+
+?>
 
 <main>
     <section class="pos-relative" style="height:100vh;">
         <figure style="height:100%;">
             <img src="{$path.images}home.jpg" class="img-cover">
         </figure>
-        <div class="pos-absolute d-flex align-items-end p-20 p-md-40" style="top:0px;right:0px;bottom:0px;left:0px;background-color:rgba(0,0,0,0.2);">
+        <div class="pos-absolute d-flex align-items-end p-20 p-md-40" style="top:0px;right:0px;bottom:0px;left:0px;background-color:rgba(0,0,0,0.4);">
             <div class="container">
                 <h1 class="m-b-20 text-center text-light">{$lang.hm_tt_1}</h1>
                 <p class="m-b-20 text-center text-light">{$lang.hm_tt_2}</p>
@@ -15,4 +21,11 @@
             </div>
         </div>
     </section>
+    <form name="contact_us">
+        <input type="text" name="name" placeholder="{$lang.name}">
+        <input type="email" name="email" placeholder="{$lang.email}">
+        <input type="text" name="phone" placeholder="{$lang.phone}">
+        <textarea name="message" placeholder="{$lang.message}"></textarea>
+        <button type="submit">{$lang.submit}</button>
+    </form>
 </main>
