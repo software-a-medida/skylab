fe36a4e663133751b3fca682d07cf8c3efb7f50c
Gersón Gómez <gergomez18@gmail.com>
2021-05-29 15:34:28 -0500

    Actualización

--- libraries/Languages.class.php
+++ libraries/Languages.class.php
@@ -9,148 +9,148 @@ class Languages
         $langs = [
             'thanks_for_schedule_an_appointment_with_us' => [
                 'es' => 'Gracias por agendar tu cita con nosotros',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Thank you for scheduling your appointment with us.',
+                'pr' => 'Obrigado por agendar sua consulta conosco.'
             ],
             'your_appointment' => [
                 'es' => 'Tu cita',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Your date',
+                'pr' => 'Seu encontro'
             ],
             'your_token' => [
                 'es' => 'Tu folio',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Your folio',
+                'pr' => 'Seu fólio'
             ],
             'test_type' => [
                 'es' => 'Tipo de prueba',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Type of test',
+                'pr' => 'Tipo de teste'
             ],
             'covid_pcr' => [
                 'es' => 'COVID-19 PCR',
-                'en' => '',
-                'pr' => ''
+                'en' => 'COVID-19 PCR',
+                'pr' => 'PCR COVID-19'
             ],
             'covid_an' => [
                 'es' => 'COVID-19 Antígenos',
-                'en' => '',
-                'pr' => ''
+                'en' => 'COVID-19 Antigens',
+                'pr' => 'Antígenos COVID-19'
             ],
             'covid_ac' => [
                 'es' => 'COVID-19 Anticuerpos',
-                'en' => '',
-                'pr' => ''
+                'en' => 'COVID-19 Antibodies',
+                'pr' => 'Anticorpos COVID-19'
             ],
             'tests_number' => [
-                'es' => 'Numero de pruebas',
-                'en' => '',
-                'pr' => ''
+                'es' => 'Número de pruebas',
+                'en' => 'Number of tests',
+                'pr' => 'Número de testes'
             ],
             'test_place' => [
                 'es' => 'Lugar de prueba',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Test site',
+                'pr' => 'Site de teste'
             ],
             'i_will_go' => [
                 'es' => 'Yo iré a la toma',
-                'en' => '',
-                'pr' => ''
+                'en' => 'I will go to the take',
+                'pr' => 'Eu irei para o soquete'
             ],
             'i_want_to_do_in_my_hotel' => [
                 'es' => 'Quiero que vengan a mi hotel a realizar la toma',
-                'en' => '',
-                'pr' => ''
+                'en' => 'I want you to come to my hotel to take the shot',
+                'pr' => 'Quero que venha ao meu hotel para tirar a foto'
             ],
             'i_want_them_to_come_look_for_me' => [
                 'es' => 'Quiero que me vengan a buscar a mi hotel para llevarme a la toma',
-                'en' => '',
-                'pr' => ''
+                'en' => 'I want to be picked up at my hotel to take me to the shot',
+                'pr' => 'Eu quero ser pego no meu hotel para me levar para a foto'
             ],
             'test_zone' => [
                 'es' => 'Zona de prueba',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Test zone',
+                'pr' => 'Zona de teste'
             ],
             'cancun_zh' => [
                 'es' => 'Cancún Zona Hotelera',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Cancun Hotel Zone',
+                'pr' => 'Cancun Hotel Zone'
             ],
             'cancun_ib' => [
                 'es' => 'Cancún Isla Blanca',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Cancun Isla Blanca',
+                'pr' => 'Cancun Isla Blanca'
             ],
             'isla_mujeres' => [
                 'es' => 'Isla Mujeres',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Isla Mujeres',
+                'pr' => 'Isla Mujeres'
             ],
             'playa_del_carmen' => [
                 'es' => 'Playa del Carmen',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Playa del Carmen',
+                'pr' => 'Playa del Carmen'
             ],
             'tulum' => [
                 'es' => 'Tulúm',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Tulum',
+                'pr' => 'Tulum'
             ],
             'riviera_maya_hotels' => [
                 'es' => 'Hoteles en la Riviera Maya',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Hotels in the Riviera Maya',
+                'pr' => 'Hotéis na Riviera Maya'
             ],
             'your_hotel' => [
                 'es' => 'Tu hotel',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Your hotel',
+                'pr' => 'Seu hotel'
             ],
             'test_date' => [
                 'es' => 'Fecha de toma',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Date taken',
+                'pr' => 'Data da tomada'
             ],
             'test_hour' => [
                 'es' => 'Hora de toma',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Take time',
+                'pr' => 'Tome um tempo'
             ],
             'first_and_last_name' => [
                 'es' => 'Nombre y apellidos',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Name and surname',
+                'pr' => 'Nomes e sobrenomes'
             ],
             'email' => [
                 'es' => 'Correo electrónico',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Email',
+                'pr' => 'Correio eletrônico'
             ],
             'phone' => [
                 'es' => 'Telefono',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Phone',
+                'pr' => 'Telefone'
             ],
             'please_use_your_face_mask' => [
                 'es' => 'Porfavor usa en todo momento tu cubrebocas y manten tu sana distancia ¡Vamos a cuidarnos entre todos! Dentro de la toma, únicamente podrán ingresar los pacientes que se realizaran la prueba.',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Please wear your mask at all times and keep your distance. Lets take care of each other! Within the intake, only the patients who will undergo the test will be able to enter.',
+                'pr' => 'Por favor, use sua máscara o tempo todo e mantenha distância. Vamos cuidar uns dos outros! Dentro da ingestão, apenas os pacientes que farão o teste poderão entrar.'
             ],
             'vcard' => [
                 'es' => 'Tarjeta de contacto',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Contact card',
+                'pr' => 'Cartão de contato'
             ],
             'power_by' => [
-                'es' => 'Poder por',
-                'en' => '',
-                'pr' => ''
+                'es' => 'Power by',
+                'en' => 'Power by',
+                'pr' => 'Power by'
             ],
             'development_by' => [
                 'es' => 'Desarrollado por',
-                'en' => '',
-                'pr' => ''
+                'en' => 'Developed by',
+                'pr' => 'Desenvolvido por'
             ]
         ];
 
